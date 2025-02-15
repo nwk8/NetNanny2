acc19ccedf2161216ff0a70cc9eb7563235c5ad3
nwk8 <nkanyi24@gmail.com>
2025-02-15 17:07:10 +0000

    tempo routes fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Suspense } from "react";
 import { useRoutes, Routes, Route } from "react-router-dom";
 import Home from "./components/home";
-import routes from "tempo-routes";
+import routes from "./tempo-routes";
 
 function App() {
   return (
